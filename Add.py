587c2a17daf7644729e77b28e4c70ef617df5d4f
madhu.97mca <madhu.97mca@gmail.com>
2025-01-14 15:33:49 +0530

    python code

---
+++ Add.py
@@ -0,0 +1,7 @@
+print("program to print the sum of two numbers")
+a=int(input('Enter the 1 Number:-'))
+b=int(input('Enter the 2 Number:-'))
+sum=a+b
+
+#print('The sum of two numbers is'+sum)
+print(f'The sum of two numbers is {sum}')
